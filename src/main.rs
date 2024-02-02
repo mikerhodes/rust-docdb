3fdf2ef4a1aa203503ba6cd89e5f89638c93b278
Mike Rhodes <mike.rhodes@gmail.com>
2024-02-02 10:31:23 +0000

    Add support for arrays in get_path_values; we'll figure how to index them later

--- src/main.rs
+++ src/main.rs
@@ -28,26 +28,40 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Clone)]
+enum PathComponent {
+    String(String),
+    ArrayIndex(usize),
+}
 // get_path_values returns a Vector of (path, value) tuples. We use the json_serde::Value type
 // so we carry around some type information for later encoding.
 // v is moved into get_path_values and any needed Values end up moved into the function's return value
-// For array support, we would put the array index into the path. Therefore we'd need to create an
-// enum that could hold either String or int and it would be the type in the path vector returned.
-fn get_path_values(v: Value) -> Vec<(Vec<String>, Value)> {
+fn get_path_values(v: Value) -> Vec<(Vec<PathComponent>, Value)> {
     let mut acc = vec![];
     let mut stack = vec![(v, vec![])];
 
     while stack.len() > 0 {
         let (v, path) = stack.pop().unwrap();
         match v {
-            Value::Array(a) => println!("found an array (that we don't support yet) {:?}", a),
+            Value::Array(a) => {
+                for (i, v) in a.into_iter().enumerate() {
+                    let mut p = path.clone();
+                    p.push(PathComponent::ArrayIndex(i));
+                    stack.push((v, p))
+                }
+            }
             Value::Bool(_) => acc.push((path, v)),
             Value::Null => acc.push((path, v)),
             Value::Number(_) => acc.push((path, v)),
             Value::Object(o) => {
+                // we consume o, and move k and v into
+                // our stack, and eventually to the
+                // accumulator. I think we have to clone
+                // path so we don't just add all the ks
+                // to the same path.
                 for (k, v) in o {
                     let mut p = path.clone();
-                    p.push(k.clone());
+                    p.push(PathComponent::String(k));
                     stack.push((v, p))
                 }
             }
